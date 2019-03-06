d29f066ece86311953b987d7e10a7a1962eb949c
Sherry2Wei <45139258+Sherry2Wei@users.noreply.github.com>
2019-03-06 16:34:43 +0800

    Update NLPTA.py

--- NLPTA.py
+++ NLPTA.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
+
 
 
 import pandas as pd
@@ -11,72 +12,267 @@ from dfply import *
 import os
 #%%
 
-
-# In[2]:
-
-
 from sklearn.feature_extraction.text import CountVectorizer
-
-
-# In[12]:
-
 
 from nltk.stem import WordNetLemmatizer
 
-
-# In[3]:
-
-
 path = r'D:\lecture\NLPTA\project\FOMC minutes\1993'
-
-
-# In[4]:
-
 
 os.chdir(path)
 
-
-# In[5]:
-
-
 file_names = os.listdir(path)
-
 file_type = ['txt' in file_name for file_name in file_names]
-
 df_file = pd.DataFrame({'name':file_names,'type':file_type},)
-
 files_93 = df_file.query('type == True')
-
 content = [open(file).read() for file in files_93['name']]
-
 def data_clean(data):##clean special characters
     return re.sub("\d+|\-|\n|/|:", "", data)
-
 content = [data_clean(open(file).read()) for file in files_93['name']]
-
-
-# In[6]:
-
-
 files_93['content'] = content
-
-
-# In[7]:
-
-
 files_93
-
 files93 = files_93.reset_index()
 files93.pop('index')
 
+files93['day'] = files93['name'].apply(lambda x : re.findall('\d+',x)[0][-2:])
+files93['month'] = files93['name'].apply(lambda x : re.findall('\d+',x)[0][-4:-2])
+files93['year'] = files93['name'].apply(lambda x : re.findall('\d+',x)[0][:-4])
+files93 = files93[['year','month','day','name','type','content']]
 
-# In[8]:
+files93
+
+
+# ### <font face = 'Times New Roman'> Lemmatize
+
+# In[380]:
+
+
+wnl = WordNetLemmatizer()
+
+def lem_term(x):
+    tokens = word_tokenize(x)
+    lem_token= [wnl.lemmatize(word) for word in tokens]
+    for char in (['.',',']):
+        while char in lem_token:
+            lem_token.remove(char)
+    content_lem = ' '.join(lem_token)
+    return content_lem
+
+
+# In[385]:
 
 
 files93
 
 
-# ### bag of words
+# In[383]:
+
+
+get_ipython().run_cell_magic('time', '', "corpus = files93['content'].apply(lem_term).tolist()")
+
+
+# In[384]:
+
+
+corpus
+
+
+# ### sklearn
+
+# ### <font face = 'Times New Roman'> Get BoW with sklearn
+
+# In[169]:
+
+
+vectorizer = CountVectorizer(stop_words = 'english',lowercase = True)
+
+
+# In[316]:
+
+
+AnuualBow = vectorizer.fit_transform(corpus)
+
+
+# In[317]:
+
+
+df_AnuualBow = pd.DataFrame(AnuualBow.A,columns = vectorizer.get_feature_names())
+
+
+# In[318]:
+
+
+files93_BoW_sk2 = pd.concat([files93,df_AnuualBow],axis = 1)
+
+
+# ### <font face = 'Times New Roman'> Get tf-df with sklearn
+
+# In[178]:
+
+
+v = TfidfVectorizer(stop_words='english', max_df=0.9)
+
+
+# In[321]:
+
+
+tfidf = v.fit_transform(corpus)
+
+
+# In[322]:
+
+
+df_Anuualtfidf = pd.DataFrame(tfidf.A,columns = v.get_feature_names())
+
+
+# In[323]:
+
+
+files93_tfidf_sk = pd.concat([files93,df_Anuualtfidf],axis = 1)
+
+
+# In[324]:
+
+
+files93_tfidf_sk
+
+
+# ### <font face = 'Times New Roman'> Get BoW with nltk
+
+# In[24]:
+
+
+from nltk.tokenize import word_tokenize
+
+
+# In[32]:
+
+
+from nltk.corpus import stopwords
+
+
+# In[172]:
+
+
+tokens = [w for w in word_tokenize(contents[0].lower()) if w.isalpha()] ## isalpha 判断是否为英文字符
+
+
+# In[173]:
+
+
+no_stops = [t for t in tokens if t not in stopwords.words('english')]
+
+
+# In[174]:
+
+
+np.array(Counter(no_stops).most_common(10))
+
+
+# In[54]:
+
+
+df = pd.DataFrame(np.array(Counter(no_stops).most_common(10)),columns = ['words','occurency'])
+
+
+# In[55]:
+
+
+df
+
+
+# In[17]:
+
+
+from collections import Counter
+
+
+# In[46]:
+
+
+Counter(tokens).most_common(2)
+
+
+# ### lemmatize
+# 
+# 
+
+# In[270]:
+
+
+lem_term(contents[0])
+
+
+# In[264]:
+
+
+files['']
+
+
+# In[263]:
+
+
+tokens = word_tokenize(contents[0])
+
+
+# In[265]:
+
+
+lem_token= [wnl.lemmatize(word) for word in tokens]
+
+
+# In[275]:
+
+
+lem_token.remove(',')
+
+
+# In[281]:
+
+
+
+
+
+# In[282]:
+
+
+lem_token
+
+
+# In[273]:
+
+
+re.sub('.|/,','',lem_token)
+
+
+# In[253]:
+
+
+lem_term(files93['content'][0])
+
+
+# In[251]:
+
+
+[lem_term(files93['content'][i]) for i in range(len(files93))]
+
+
+# In[248]:
+
+
+files93['content'].transform(lambda x:lem_term(x))
+
+
+# In[254]:
+
+
+word_tokenize(contents[0])
+
+
+# In[236]:
+
+
+bow = vectorizer.fit_transform(lem_token)
+
 
 # In[ ]:
 
@@ -84,50 +280,34 @@ files93
 
 
 
-# In[9]:
+# In[237]:
 
 
-vectorizer = CountVectorizer(stop_words = 'english',lowercase = True)
+len(AnuualBow.A[0])
 
 
-# In[10]:
+# In[238]:
 
 
-X = vectorizer.fit_transform([files93['content'][0]])
+len(bow.A[0])
 
 
-# In[13]:
-
-
-tokens = vectorizer.get_feature_names()
-
-
-# ### lemmatize
-# 
-# 
-
-# In[15]:
-
-
-wnl = WordNetLemmatizer()
-
-
-# In[19]:
+# In[201]:
 
 
 len(tokens)
 
 
-# In[18]:
+# In[204]:
 
 
-len([wnl.lemmatize(word) for word in tokens])
+x = vectorizer.fit_transform([contents[0]])
 
 
-# In[17]:
+# In[209]:
 
 
-files93
+len(vectorizer.get_feature_names())
 
 
 # In[ ]:
