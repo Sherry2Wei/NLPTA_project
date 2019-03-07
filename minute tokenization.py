af9679d43b724df5398acad19b7bb6a813f737ac
Sherry2Wei <45139258+Sherry2Wei@users.noreply.github.com>
2019-03-07 10:17:30 +0800

    Update minute tokenization.py

--- minute tokenization.py
+++ minute tokenization.py
@@ -3,10 +3,10 @@ from dfply import *
 import time
 import pandas as pd
 import re
-#from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-#from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from multiprocessing import Pool
 
 
@@ -14,12 +14,6 @@ def readfile(filename):
     file = open(path + os.sep + filename, encoding="utf8")
     file_contents = file.read()
     return file_contents
-
-
-def add(x):
-    x = x+1
-    return x
-
 
 def lem_term(document):
     wnl = WordNetLemmatizer()
@@ -52,7 +46,40 @@ if __name__ == '__main__':
     print(doc_df >> head(5))
     print("--- %s seconds ---" % (time.time() - start_time))
 
+wnl = WordNetLemmatizer()
 
+def lem_term(x):
+    tokens = word_tokenize(x)
+    lem_token= [wnl.lemmatize(word) for word in tokens]
+    for char in (['.',',']):
+        while char in lem_token:
+            lem_token.remove(char)
+    content_lem = ' '.join(lem_token)
+    return content_lem
+
+corpus = doc_df['content'].apply(lem_term).tolist()
+#%% bag of words with sklearn
+vectorizer = CountVectorizer(stop_words = 'english',lowercase = True)
+AnuualBow = vectorizer.fit_transform(corpus)
+df_AnuualBow = pd.DataFrame(AnuualBow.A,columns = vectorizer.get_feature_names())
+doc_df_BoW_sk2 = pd.concat([doc_df,df_AnuualBow],axis = 1)
+#%% tf-idf sklearn
+v = TfidfVectorizer(stop_words='english', max_df=0.9)
+tfidf = v.fit_transform(corpus)
+df_Anuualtfidf = pd.DataFrame(tfidf.A,columns = v.get_feature_names())
+doc_df_tfidf_sk = pd.concat([doc_df,df_Anuualtfidf],axis = 1)
+doc_df_tfidf_sk
+
+#%% bag of word with nltk
+from collections import Counter
+from nltk.corpus import stopwords
+def get_bow_nltk(content):
+    tokens = [w for w in word_tokenize(content.lower()) if w.isalpha()]
+    no_stops = [t for t in tokens if t not in stopwords.words('english')]
+    bow_nltk = Counter(no_stops).most_common(len(no_stops))
+    df_bow_nltk = pd.DataFrame(bow_nltk,columns = ['terms','occurrences'])
+    df_bow_nltk['content'] = content*len(no_stops)
+    return df_bow_nltk
 
 
 
