2ef1aba26afa79086a4576604e5332404b5dd46a
Sherry2Wei <45139258+Sherry2Wei@users.noreply.github.com>
2019-03-07 14:51:25 +0800

    Update minute tokenization.py

--- minute tokenization.py
+++ minute tokenization.py
@@ -8,7 +8,8 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from multiprocessing import Pool
-
+import warnings
+warnings.filterwarnings("ignore",category=DeprecationWarning)
 
 def readfile(filename):
     file = open(path + os.sep + filename, encoding="utf8")
@@ -23,6 +24,7 @@ def lem_term(document):
         while char in lem_token:
             lem_token.remove(char)
     document = ' '.join(lem_token)
+    re.sub('\+d','',document)
     return document
 
 
@@ -46,51 +48,46 @@ if __name__ == '__main__':
     print(doc_df >> head(5))
     print("--- %s seconds ---" % (time.time() - start_time))
 
-wnl = WordNetLemmatizer()
+    doc_df.to_csv('doc_df.csv')
+    wnl = WordNetLemmatizer()
 
-def lem_term(x):
-    tokens = word_tokenize(x)
-    lem_token= [wnl.lemmatize(word) for word in tokens]
-    for char in (['.',',']):
-        while char in lem_token:
-            lem_token.remove(char)
-    content_lem = ' '.join(lem_token)
-    return content_lem
-
-corpus = doc_df['content'].apply(lem_term).tolist()
-#%% bag of words with sklearn
-vectorizer = CountVectorizer(stop_words = 'english',lowercase = True)
-AnuualBow = vectorizer.fit_transform(corpus)
-df_AnuualBow = pd.DataFrame(AnuualBow.A,columns = vectorizer.get_feature_names())
-doc_df_BoW_sk2 = pd.concat([doc_df,df_AnuualBow],axis = 1)
-#%% tf-idf sklearn
-v = TfidfVectorizer(stop_words='english', max_df=0.9)
-tfidf = v.fit_transform(corpus)
-df_Anuualtfidf = pd.DataFrame(tfidf.A,columns = v.get_feature_names())
-doc_df_tfidf_sk = pd.concat([doc_df,df_Anuualtfidf],axis = 1)
-doc_df_tfidf_sk
-
-#%% bag of word with nltk
-from collections import Counter
-from nltk.corpus import stopwords
-def get_bow_nltk(content):
-    tokens = [w for w in word_tokenize(content.lower()) if w.isalpha()]
-    no_stops = [t for t in tokens if t not in stopwords.words('english')]
-    bow_nltk = Counter(no_stops).most_common(len(no_stops))
-    df_bow_nltk = pd.DataFrame(bow_nltk,columns = ['terms','occurrences'])
-    df_bow_nltk['content'] = content*len(no_stops)
-    return df_bow_nltk
+    corpus = doc_df['content'].apply(lem_term).tolist()
+    #%% bag of words with sklearn
+    vectorizer = CountVectorizer(stop_words = 'english',lowercase = True)
+    AnuualBow = vectorizer.fit_transform(corpus)
+    df_AnuualBow = pd.DataFrame(AnuualBow.A,columns = vectorizer.get_feature_names())
+    doc_df_BoW_sk2 = pd.concat([doc_df,df_AnuualBow],axis = 1)
+    #%% tf-idf sklearn
+    v = TfidfVectorizer(stop_words='english', max_df=0.9)
+    tfidf = v.fit_transform(corpus)
+    df_Anuualtfidf = pd.DataFrame(tfidf.A,columns = v.get_feature_names())
+    doc_df_tfidf_sk = pd.concat([doc_df,df_Anuualtfidf],axis = 1)
+    doc_df_tfidf_sk
 
 
+    #%% bag of word with nltk
+    from collections import Counter
+    from nltk.corpus import stopwords
+    def get_bow_nltk(content):
+        tokens = [w for w in word_tokenize(content.lower()) if w.isalpha()]
+        no_stops = [t for t in tokens if t not in stopwords.words('english')]
+        bow_nltk = Counter(no_stops).most_common(len(no_stops))
+        df_bow_nltk = pd.DataFrame(bow_nltk,columns = ['terms','occurrences'])
+        df_bow_nltk['content'] = content*len(no_stops)
+        return df_bow_nltk
 
-IR = pd.read_csv(r'D:\lecture\NLPTA\project\FEDRateChange.csv')
-IR >> head(3)
+    IR = pd.read_csv(r'D:\lecture\NLPTA\project\FEDRateChange.csv')
+    IR >> head(3)
+    IR.columns
+    IR.rename(columns={ IR.columns[2]: "day" }, inplace=True)
+    IR['IR_change_freq_Annual'] = IR.groupby(['Year'])['IR_Change'].transform('count')
 
-IR['IR_change_freq_Annual'] = IR.groupby(['year'])['IR_Change'].transform('count')
-IR[IR['IR_change_freq_Annual'] == IR['IR_change_freq'].max()]
+    IR[IR['IR_change_freq_Annual'] == IR['IR_change_freq_Annual'].max()]
 
-#%% merge interest rate with terms frame
-
-
-
+    #%% merge interest rate with terms frame
+    com_col = set(IR.columns&doc_df_BoW_sk2.columns)
+    #pd.merge(IR,df_AnuualBow,on = list(com_col),how = 'outer')
+    #len(df_AnuualBow)
+    #Counter(doc_df_BoW_sk2.columns.tolist())
+    #len(doc_df_BoW_sk2.columns)
 
